f960f1fd0c11a08767ee28bdb999abc5c13ce4b6
chandrac-01 <cchatterjee108.com>
2025-02-03 14:04:42 -0500

    message

---
+++ restaurantMenu/restaurant_menu.js
@@ -0,0 +1,20 @@
+
+const breakfastMenu = ['Pancakes- $12', 'Eggs Benedict -$22.99', 'Oatmeal -$21.99', 'Frittata -$15'];
+const mainCourseMenu = ['Steak- $20', 'Pasta- $50', 'Burger- $15.48', 'Salmon- $70.35'];
+const dessertMenu = ['Cake- $20', 'Ice Cream- $5', 'Pudding- $15.22', 'Fruit Salad- $0.34'];
+
+// breakfast
+const breakfastMenuItemsHTML = breakfastMenu.map((item, index) => `<p>Item ${index + 1}: ${item}</p>`).join('');
+document.getElementById('breakfastMenuItems').innerHTML = breakfastMenuItemsHTML;
+
+// main course
+let mainCourseItem = '';
+mainCourseMenu.forEach((item, index) => {
+mainCourseItem += `<p>Item ${index + 1}: ${item}</p>`;});
+document.getElementById('maincourseMenuItems').innerHTML = mainCourseItem;
+
+//dessert
+let dessertItem = '';
+for (let i = 0; i < dessertMenu.length; i++) {
+    dessertItem += `<p>Item ${i + 1}: ${dessertMenu[i]}</p>`;}
+document.getElementById('dessertMenuItems').innerHTML = dessertItem;
