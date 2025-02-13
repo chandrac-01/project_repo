4ab4b4ceb7d086041368cf95908205368ccdb3a3
chandrac-01 <cchatterjee108.com>
2025-02-13 04:51:34 -0500

    message

--- weatherReport/weather_report.js
+++ weatherReport/weather_report.js
@@ -17,5 +17,9 @@ fetch(apiUrl)
 
 document.getElementById('weatherForm').addEventListener('submit',showweatherDetails );
 
-
+//.catch(error => {
+ //   console.error('Error fetching weather:', error);
+ //   const weatherInfo = document.getElementById('weatherInfo');
+ //   weatherInfo.innerHTML = `<p>Failed to fetch weather. Please try again.</p>`;
+ // });
 
